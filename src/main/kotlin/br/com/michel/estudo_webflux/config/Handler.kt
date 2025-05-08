109e195f2991ba55a0bce8126318f512ee037077
michelzarpe <michelzarpe@gmail.com>
2025-05-08 15:42:48 -0300

    handler

---
+++ src/main/kotlin/br/com/michel/estudo_webflux/config/Handler.kt
@@ -0,0 +1,24 @@
+package br.com.michel.estudo_webflux.config
+
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException
+import org.springframework.dao.DuplicateKeyException
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageConversionException
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import java.sql.SQLTransientConnectionException
+
+class Handler {
+    @ExceptionHandler(
+        DuplicateKeyException::class,
+        IllegalArgumentException::class,
+        HttpMessageNotReadableException::class,
+        InvalidDefinitionException::class,
+        HttpMessageConversionException::class,
+        DuplicateKeyException::class,
+        SQLTransientConnectionException::class
+    )
+    fun onBusinessException(e: Exception): ResponseEntity<*> {
+        return ResponseEntity.unprocessableEntity().build<String>()
+    }
+}
